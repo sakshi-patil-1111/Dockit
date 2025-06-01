81bf2176d6cda391bfe331a55595d1078c134358
Sakshi Patil <sakshispatil1111@gmail.com>
2025-06-01 23:40:51 +0530

    setup database and cloudinary

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import connectDB from "./config/mongodb.js";
+import connectCloudinary from "./config/cloudinary.js";
 
 const app = express();
 const PORT = process.env.PORT || 8080;
+connectDB();
+connectCloudinary();
 
 app.use(cors());
 app.use(express.json());
