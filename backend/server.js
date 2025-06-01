756fcb65357002a2325fe4436bc5dd26bb170566
Sakshi Patil <sakshispatil1111@gmail.com>
2025-06-02 02:35:33 +0530

    add doctor and auth for admin

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import "dotenv/config";
 import connectDB from "./config/mongodb.js";
 import connectCloudinary from "./config/cloudinary.js";
+import adminRouter from "./routes/adminRoute.js";
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -11,6 +12,8 @@ connectCloudinary();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/admin", adminRouter);
 
 app.get("/", (req, res) => {
   res.send("API Working");
