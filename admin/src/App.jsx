b5cfbd6313e82ac1ed273ce4ddedf06571f3e131
Sakshi Patil <sakshispatil1111@gmail.com>
2025-06-02 17:13:16 +0530

    navbar and sidebar

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -3,12 +3,30 @@ import Login from "./pages/Login";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { AdminContext } from "./context/AdminContext";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import { Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Admin/Dashboard";
+import AllAppointments from "./pages/Admin/AllAppointments";
+import DoctorsList from "./pages/Admin/DoctorsList";
+import AddDoctor from "./pages/Admin/AddDoctor";
 
 const App = () => {
   const { aToken } = useContext(AdminContext);
   return aToken ? (
-    <div>
+    <div className="bg-[#F8F9FD]">
       <ToastContainer />
+      <Navbar />
+      <div className="flex items-start">
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={<></>} />
+          <Route path="/admin-dashboard" element={<Dashboard />} />
+          <Route path="/all-appointments" element={<AllAppointments />} />
+          <Route path="/doctor-list" element={<DoctorsList />} />
+          <Route path="/add-doctor" element={<AddDoctor />} />
+        </Routes>
+      </div>
     </div>
   ) : (
     <div>
